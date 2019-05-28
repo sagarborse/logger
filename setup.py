2422791f40a9ae901d44630f691ca457daddb7bd
Sagar Borse <sagarborse@EIMLSPRM103.local>
2019-05-28 15:05:52 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eros-logger',
+    packages=['eros-logger'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='http://github.com/example/linode_example',
+    author='Sagar Borse',
+    author_email='sagarborse90@gmail.com',
+    keywords=['pip','eros logger','logger']
+)
