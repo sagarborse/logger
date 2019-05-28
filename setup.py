23d33e49a49f6409d17346d22a163a56aed950ea
Sagar Borse <sagarborse@EIMLSPRM103.local>
2019-05-28 15:49:52 +0530

    project initialization

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-setup(
-    name='eros-logger',
-    packages=['eros-logger'],
-    description='Hello world enterprise edition',
-    version='0.1',
-    url='http://github.com/example/linode_example',
-    author='Sagar Borse',
-    author_email='sagarborse90@gmail.com',
-    keywords=['pip','eros logger','logger']
-)
+setup(name='eros_logger',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sagarborse/logger',
+      author='Sagar Borse',
+      author_email='sagarborse90@gmail.com',
+      license='MIT',
+      packages=['logger'],
+      zip_safe=False)
