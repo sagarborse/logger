74a77fd25ee2ab55ada5c6b242ee4690cfd4daec
Sagar Borse <sagarborse@EIMLSPRM103.local>
2019-05-29 01:35:38 +0530

    changes

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,3 +1,3 @@
-from .logger  import Logger
+from logger_settings  import *
 
 __version__ = "1.0.0"
--- logger/logger_settings.py
+++ logger/logger_settings.py
@@ -2,65 +2,65 @@ import structlog
 
 
 LOGGING = {
-    'version':1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "json_formatter": {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processor": structlog.processors.JSONRenderer(),
         },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
+        "plain_console": {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processor": structlog.dev.ConsoleRenderer(),
         },
-    },
-    'formatters':{
-        'large':{
-            'format':'%(asctime)s  %(levelname)s  %(process)d  %(pathname)s  %(funcName)s  %(lineno)d  %(message)s  '
-        },
-        'tiny':{
-            'format':'%(asctime)s  %(message)s  '
-        }
-    },
-    'handlers':{
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-            'formatter': 'large'
-        },
-        'logfile':{
-            'level':'INFO',
-               'class':'logging.handlers.TimedRotatingFileHandler',
-            'when':'midnight',
-            'interval':1,
-            'backupCount': 10,
-            'filename': os.path.join(MAIN_DIR, 'logs/commservice.log'),
-            'formatter':'large',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers':{
-        'django': {
-            'handlers': ['console', 'mail_admins'],
-            'propagate': True,
-        },
-        'django.db.backends': {
-            'handlers': ['logfile'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['logfile'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'custom_logger':{
-            'handlers':['logfile'],
-            'level':'DEBUG',
-            'propagate':False,
+        "key_value": {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processor": structlog.processors.KeyValueRenderer(key_order=['timestamp', 'level', 'event', 'logger']),
         },
     },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "plain_console",
+        },
+        "json_file": {
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": "logs/json.log",
+            "formatter": "json_formatter",
+        },
+        "flat_line_file": {
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": "logs/flat_line.log",
+            "formatter": "key_value",
+        },
+    },
+    "loggers": {
+        "django_structlog": {
+            "handlers": ["console", "flat_line_file", "json_file"],
+            "level": "INFO",
+        },
+        "django_structlog_demo_project": {
+            "handlers": ["console", "flat_line_file", "json_file"],
+            "level": "INFO",
+        },
+    }
 }
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.processors.ExceptionPrettyPrinter(),
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ],
+    context_class=structlog.threadlocal.wrap_dict(dict),
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
